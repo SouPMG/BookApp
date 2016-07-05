fc2ff992928cd3251740919ecd1fedd96c7a3084
Mattia Giacobbe <mattia.giacobbe.1993@gmail.com>
2016-07-05 21:58:34 +0200

    Started working on mainwindow

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
-/*#include "mainwindow.h"
+#include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
 	QApplication a(argc, argv);
+
 	MainWindow w;
 	w.show();
 
 	return a.exec();
-}
-*/
-
-#include <iostream>
-#include <string>
-
-#include "book.h"
-
-using namespace std;
-
-int main(int argc, char *argv[]) {
-	book *myBook = new book ("ISBN");
-	cout << myBook->getIsbn() << endl;
 }
