cc47f6f04863dc114294c3e96f1066d101a241e2
Mattia Giacobbe <mattia.giacobbe.1993@gmail.com>
2016-08-15 09:38:53 +0200

    Finished library container implementation

--- library.cpp
+++ library.cpp
@@ -1,21 +1,54 @@
 #include "library.h"
 
-library::library() : first(0) {}
+Library::Node::Node() : next(0) {}
 
-library::node::node() : next(0) {}
+Library::Node::Node(LibraryItem *item, Node *n) : info(item), next(n) {}
 
-library::node::node(libraryItem *item, node *n) : info(item), next(n) {}
+Library::Library() : first(0) {}
 
-bool library::empty() const {
+// deep copy constructor using private method copy
+Library::Library(const Library &l) : first(copy(l.first)) {}
+
+Library::Node* Library::copy(Node *n) {
+	if (!n) {
+		return 0;
+	} else {
+		return new Node (n->info, copy(n->next));
+	}
+}
+
+// deep destructor using private method destroy
+Library::~Library() {
+	destroy(first);
+}
+
+void Library::destroy(Node *n) {
+	if (n) {
+		destroy(n->next);
+		delete n;
+	}
+}
+
+bool Library::empty() const {
 	return first == 0;
 }
 
-void library::addItem(libraryItem *newItem) {
-	first = new node(newItem, first);
+int Library::size() const {
+	int librarySize = 0;
+	Node *temp = first;
+	while (temp) {
+		librarySize++;
+		temp = temp->next;
+	}
+	return librarySize;
 }
 
-void library::removeItem(libraryItem *item) {
-	node *n = first, *prev = 0;
+void Library::addItem(LibraryItem *newItem) {
+	first = new Node(newItem, first);
+}
+
+void Library::removeItem(LibraryItem *item) {
+	Node *n = first, *prev = 0;
 
 	while (n && !(n->info == item)) {
 		prev = n;
@@ -32,10 +65,76 @@ void library::removeItem(libraryItem *item) {
 	}
 }
 
-libraryItem* library::extract() {
-	node* n = first;
+LibraryItem* Library::extract() {
+	Node* n = first;
 	first = first->next;
-	libraryItem* aux = n->info;
+	LibraryItem* aux = n->info;
 	delete n;
 	return aux;
+}
+
+LibraryItem *Library::at(int index) const {
+	Node *temp = first;
+	int counter = 0;
+	while (counter < index) {
+		counter++;
+		temp = temp->next;
+	}
+	if (temp) {
+		return temp->info;
+	} else {
+		return NULL;
+	}
+
+}
+
+Library& Library::operator =(const Library &l) {
+	if (this != &l) { // operator!= between pointers
+		destroy(first);
+		first = copy(l.first);
+	}
+	return *this;
+}
+
+// LibraryIterator methods implementation
+bool Library::LibraryIterator::operator ==(LibraryIterator iterator) const {
+	return pointer == iterator.pointer;
+}
+
+bool Library::LibraryIterator::operator !=(LibraryIterator iterator) const {
+	return pointer != iterator.pointer;
+}
+
+Library::LibraryIterator& Library::LibraryIterator::operator ++() {
+	// prefix increase
+	if (pointer) {
+		pointer = pointer->next; // side effect
+	}
+	return *this;
+}
+
+Library::LibraryIterator Library::LibraryIterator::operator ++(int) {
+	// postfix increase
+	LibraryIterator aux = *this;
+	if (pointer) {
+		pointer = pointer->next;
+	}
+	return aux;
+}
+
+Library::LibraryIterator Library::begin() const {
+	LibraryIterator aux;
+	aux.pointer = first;
+	return aux;
+}
+
+Library::LibraryIterator Library::end() const {
+	LibraryIterator aux;
+	aux.pointer = 0;
+	return aux;
+}
+
+LibraryItem* Library::operator [](Library::LibraryIterator iterator) const {
+	return (iterator.pointer)->info;
+	// NB: no i.pointer != 0 checks
 }
--- library.h
+++ library.h
@@ -3,24 +3,54 @@
 
 #include "libraryitem.h"
 
-class library {
+class Library {
+	friend class LibraryIterator;
 	private:
 		// internal private class node
-		class node {
+		class Node {
 			public:
-				node();
-				node(libraryItem*, node*);
-				libraryItem* info; // pointer to pure virtual base class
-				node* next;
+				Node();
+				Node(LibraryItem*, Node*);
+				LibraryItem* info; // pointer to pure virtual base class
+				Node* next;
+				//~Node();
 		};
-		node* first;
+		Node* first; // pointer to the first node
+		static Node* copy(Node*); // deep copy utility method
+		static void destroy(Node*); // deep destructor utility method
 
 	public:
-		library();
+		// internal public iterator class
+		class LibraryIterator {
+			friend class Library;
+			private:
+				Library::Node* pointer; // node pointed by iterator
+			public:
+				bool operator ==(LibraryIterator) const;
+				bool operator !=(LibraryIterator) const;
+				LibraryIterator& operator ++(); // prefix
+				LibraryIterator operator ++(int); // postfix
+		};
+
+		Library();
+		Library(const Library&); // deep copy
+		~Library(); // deep destructor
+
 		bool empty() const;
-		void addItem(libraryItem*);
-		void removeItem(libraryItem*);
-		libraryItem* extract();
+		int size() const;
+		LibraryItem* at(int) const; // get library item at a given index
+		void addItem(LibraryItem*);
+		//void addItemAt(int, LibraryItem*);
+		void removeItem(LibraryItem*);
+		LibraryItem* extract();
+
+		//Operators
+		Library& operator =(const Library&); // deep allocation
+
+		// methods that use LibraryIterator
+		LibraryIterator begin() const;
+		LibraryIterator end() const;
+		LibraryItem* operator [](LibraryIterator) const;
 };
 
 #endif // LIBRARY_H
